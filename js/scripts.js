17b8e252d7f3fd95b6056a1b1431b8b230a5dd02
Kyle Pratuch & Jordan Navratil <student@epicodus.com>
2015-09-10 11:18:20 -0700

    add page w/ jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,21 @@ var countUp = function(upTo, multiple) {
 
     var counter = 0;
     var numbers = [];
+
     while (counter < upTo) {
         counter += multiple;
         numbers.push(counter);
     }
     return numbers;
-}
+};
 
 $(document).ready(function() {
     $("form#count_up").submit(function(event) {
-        var upTo = $("input#up_to").val();
-        var multiple = $("input#multiple").val();
+        var upTo = parseInt($("input#up_to").val());
+        var multiple = parseInt($("input#multiple").val());
 
         var result = countUp(upTo, multiple);
-        $(".result-numbers").text(result);
+        $(".result-numbers").text(result.join(", "));
 
         $("#result").show();
         event.preventDefault();
